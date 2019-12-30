2e5e5be6ddf40018145eaa1a1eb75906b1f08751
Vitaliy Radchishin <vitaliyrd@gmail.com>
2019-12-29 20:02:51 -0800

    Refresh slide viewer page every 15 minutes

--- slides/views.py
+++ slides/views.py
@@ -4,7 +4,9 @@ from slides.models import Slide
 
 
 def viewer(request):
+    # TODO: [#1] Make durations configurable
     return render(request, 'slides/viewer.html', {
+        'refreshDuration': 900,
         'slideDuration': 5,
         'slides': Slide.objects.all(),
         'transitionDuration': 1,
