abcb851fbe81b15441a70ac838c6a68c865340ef
Vitaliy Radchishin <vitaliyrd@gmail.com>
2019-12-29 19:46:12 -0800

    Rename slide background image using UUID v4 during upload

--- slides/models.py
+++ slides/models.py
@@ -1,8 +1,15 @@
+import uuid
+
 from django.db import models
 
 
+def rename_to_uuid(model_instance, filename):
+    extension = filename.split('.')[-1]
+    return '%s.%s' % (str(uuid.uuid4()), extension)
+
+
 class Slide(models.Model):
-    background_image = models.ImageField()
+    background_image = models.ImageField(upload_to=rename_to_uuid)
     created = models.DateTimeField(auto_now_add=True)
     description = models.CharField(blank=True, max_length=1024, null=True)
     name = models.CharField(blank=True, max_length=128, null=True)
